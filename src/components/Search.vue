327915f84c7990a93c37cbd95a4ace137a65df59
thanhvu2901 <vcthanh2901@gmail.com>
2021-12-06 14:59:04 +0700

    Vue

---
+++ src/components/Search.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-form>
+    <v-text-field v-model=" searchTitle"
+                  label="What do you need to Search?"
+                value="query"
+                onChange="txt_change"
+            
+                  autofocus
+
+                  >
+    </v-text-field>
+  </v-form>
+</template>
+
+<script>
+
+
+export default {
+  name: 'SeachField',
+  data () {
+    return {
+      title: ''
+
+    }
+  },
+
+  methods: {
+    search () {
+        this.
+    }
+  }
+}
+
+</script>
